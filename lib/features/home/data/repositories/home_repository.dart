9e51a48ca6f32d06bfc6d048b0f6e2d8677fd078
Raj Kumar <rajchauhan2816@gmail.com>
2020-06-08 14:13:56 +0530

    Adds Concere Home Repository Class

---
+++ lib/features/home/data/repositories/home_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../domain/entities/student_attendance.dart';
+import '../../domain/entities/student_details.dart';
+import '../../domain/repositories/i_home_repository.dart';
+
+class HomeRepository implements IHomeRepository {
+  @override
+  Future<Either<Failure, StudentAttendance>> getStudentAttendance() {
+    // TODO: implement getStudentAttendance
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, StudentDetails>> getStudentDetails() {
+    // TODO: implement getStudentDetails
+    throw UnimplementedError();
+  }
+
+}
--- lib/features/home/domain/repositories/i_home_repository.dart
+++ lib/features/home/domain/repositories/i_home_repository.dart
@@ -1,7 +1,7 @@
-import 'package:Attendit/features/home/domain/entities/student_attendance.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/error/failures.dart';
+import '../entities/student_attendance.dart';
 import '../entities/student_details.dart';
 
 abstract class IHomeRepository {
