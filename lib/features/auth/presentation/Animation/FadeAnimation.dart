51b017234da260c7c20e7aded7faea3951d64490
Raj Kumar <rajchauhan2816@gmail.com>
2020-08-13 15:25:21 +0530

    Fixes Linter

--- lib/features/auth/presentation/Animation/FadeAnimation.dart
+++ lib/features/auth/presentation/Animation/FadeAnimation.dart
@@ -9,13 +9,18 @@ class FadeAnimation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: deprecated_member_use
     final tween = MultiTrackTween([
-      Track("opacity").add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      // ignore: deprecated_member_use
+      Track("opacity")
+          .add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      // ignore: deprecated_member_use
       Track("translateY").add(
-        Duration(milliseconds: 500), Tween(begin: -30.0, end: 0.0),
-        curve: Curves.easeOut)
+          Duration(milliseconds: 500), Tween(begin: -30.0, end: 0.0),
+          curve: Curves.easeOut)
     ]);
 
+    // ignore: deprecated_member_use
     return ControlledAnimation(
       delay: Duration(milliseconds: (500 * delay).round()),
       duration: tween.duration,
@@ -24,9 +29,7 @@ class FadeAnimation extends StatelessWidget {
       builderWithChild: (context, child, animation) => Opacity(
         opacity: animation["opacity"],
         child: Transform.translate(
-          offset: Offset(0, animation["translateY"]), 
-          child: child
-        ),
+            offset: Offset(0, animation["translateY"]), child: child),
       ),
     );
   }
